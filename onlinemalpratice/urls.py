66fc7eb9d0a54fcba3811d4c68b511b37d67cbe4
Shabeeb-ops <shabeebmuhammed510@gmail.com>
2026-06-27 23:58:58 +0530

    Fix: use django.conf.settings and absolute redirect path in urls.py

--- onlinemalpratice/urls.py
+++ onlinemalpratice/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 
-from onlinemalpratice import settings
-
 urlpatterns = [
-    path('', lambda request: redirect('myapp/admin_login/')),
+    path('', lambda request: redirect('/myapp/admin_login/')),
     path('admin/', admin.site.urls),
     path('myapp/', include('myapp.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
